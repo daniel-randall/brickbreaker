efdae3947e739e03b908cafea9ca134a3a696ad2
Justin Pallo <justinpallo@gmail.com>
2016-04-30 21:09:06 -0600

    main.js update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-var canvas = document.getElementById('game');
-var ctx = canvas.getContext('2d')
+function init() {
+  var canvas = document.getElementById('game'),
+    ctx = canvas.getContext('2d')
 
-ctx.fillStyle = "#000000";
-ctx.fillRect(100, 100, 500, 500);
+  ctx.fillStyle = "#000000";
+    ctx.fillRect(100, 100, 500, 500);   
+}
+
+document.addEventListener("DOMContentLoaded", init, false);
