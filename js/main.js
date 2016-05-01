bfc92515461d93dac1407f6277de3d151025c293
Justin Pallo <justinpallo@gmail.com>
2016-04-30 23:14:23 -0600

    update my version of main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
+class Platform{
+    constructor(locX, size){
+        this.x = locX;
+        this.size = size;
+        this.y = 50;
+        this.boxHeight = 15;
+    }
+
+    draw(ctx){
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(this.x, this.y, this.size, this.boxHeight);
+        //document.write(this.boxHeight);
+    }
+}
+
 function init() {
   var canvas = document.getElementById('game'),
       ctx = canvas.getContext('2d');
-  
+
     // autosize canvas to window size
     ctx.canvas.width  = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
-    
+
     //create new platform and draw it
-    var platform = new Platform(100, 100, 150);
+    var platform = new Platform(250, 300);
     platform.draw(ctx);
 }
 
 document.addEventListener("DOMContentLoaded", init, false);
-
-class Platform{
-    constructor(locX, locY, size){
-        this.x = locX;
-        this.y = locY;
-        this.size = size
-        
-        const this.BOX_HEIGHT = 15;
-    }
-    
-    function draw(ctx){
-        ctx.fillStyle = "#afafaf";
-        ctx.fillRect(x, y, x+size, y+this.BOX_HEIGHT);
-    }
-}
